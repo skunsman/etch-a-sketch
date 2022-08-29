0e95695465d7a460860fbcc4dc4d8e62470d14ea
Scott Kunsman <skunsman@overdrive.com>
2022-08-28 21:06:14 -0400

    Construct default 16x16 grid

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const container = document.querySelector('.container');
-container.textContent = "Hello World!";
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+
+    for (let i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement('div');
+        cell.textContent = i + 1;
+        container.appendChild(cell);
+    };
+};
+
+
+makeRows(16, 16);
