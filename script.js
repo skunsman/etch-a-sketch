5f133d2c3a85c30828271d45dd9b74817fd12edf
Scott Kunsman <skunsman@overdrive.com>
2022-08-28 22:00:22 -0400

    Add reset and changeGridSize functions

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('#reset');
+const changeGridButton = document.querySelector('#change-grid');
 
-function changeColor(evt) {
-    evt.target.className = 'fill';
+resetButton.addEventListener('click', resetGrid);
+changeGridButton.addEventListener('click', changeGridSize);
+
+
+function resetGrid(evt) {
+    const cells = container.querySelectorAll('div.fill');
+    cells.forEach(cell => {
+        cell.classList.remove('fill');
+    });
+
+    makeRows(16, 16);
 }
 
+
+function changeGridSize(evt) {
+    let gridSize = 101;
+
+    while (gridSize > 100) {
+        gridSize = Number(prompt('Select a size 1-100', 16));
+        if (gridSize === 0)
+            return;
+        if (!gridSize || gridSize <= 0)
+            gridSize = 101;
+    }
+
+    console.log(gridSize);
+    makeRows(gridSize, gridSize);
+}
+
+
+function changeColor(evt) {
+    evt.target.classList.add('fill');
+}
+
+
 function makeRows(rows, cols) {
+    container.innerHTML = '';
+
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
 
